e0756a1dbc31a2a85d43c253a4eff6d7fadc9782
Ivan Shchipin <ivan.shchipin@gmail.com>
2023-01-19 00:12:36 +0300

    add blocker with message

--- src/components/GameScreen.tsx
+++ src/components/GameScreen.tsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 import { GameStatus } from '../types/gameStatus';
+import Blocker from './play/Blocker';
 import Modal from './play/Modal';
 import PlayScreen from './play/PlayScreen';
 import SetDifficultScreen from './setDifficult/SetDifficultScreen';
@@ -19,7 +20,12 @@ const GameScreen = () => {
 						return <PlayScreen />;
 
 					case GameStatus.preGame:
-						return <PlayScreen />;
+						return (
+							<>
+								<PlayScreen />
+								<Blocker />
+							</>
+						);
 
 					case GameStatus.win:
 						return (
---
+++ src/components/play/Blocker.tsx
@@ -0,0 +1,9 @@
+const Blocker = () => {
+	return (
+		<div className="blocker">
+			<h1 className="blocker__message">Запомните карты!</h1>
+		</div>
+	);
+};
+
+export default Blocker;
